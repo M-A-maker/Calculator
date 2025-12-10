ddb21fa6d3d284babb88054ddf7e28d65e70a920
M-A-maker <mennaislam2006@gmail.com>
2025-12-10 10:01:01 +0200

    Remove commented-out code and retain mixstrings function

--- practicing.py
+++ practicing.py
@@ -1,17 +1,1 @@
-# def h_e(h,l):
-#     n=h+l
-#     return n
-# l= input("enter first number ")
-# h= input("enter second number ")
-# print(h_e(h,l))
 
-
-def mixstrings(s1,s2):
-    s2=s2[::-1]
-    s3=""
-    for i in range(len(s1)):
-        s3=s3+s1[i]+s2[i]
-    return s3
-s1=input("Please enter your first word: ")
-s2=input("Please enter your second word: ")
-print(mixstrings(s1,s2))
